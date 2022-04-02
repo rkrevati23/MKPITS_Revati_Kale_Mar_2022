ba33ac44a59cd102c81676370bb995077dfe6c4e
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-02 13:11:46 +0530

    Add files via upload

---
+++ area_ rt,c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int base,height;
+float area_rt;
+printf("enter base");
+scanf("%d",&base);
+printf("enter height");
+scanf("%d",&height);
+area_rt=(1/2)*base*height;
+printf("area of right angle triangle=%2f,area_rt");
+}
