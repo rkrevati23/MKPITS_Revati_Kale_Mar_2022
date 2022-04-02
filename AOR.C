89036a7b7983522c3c8086fe7351402838785577
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-02 09:53:13 +0530

    Add files via upload

---
+++ AOR.C
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main()
+{
+int lenght,breath,rect_area;
+printf("enter lenght");
+scanf("%d",&lenght);
+printf("enter breath");
+scanf("%d",&breath);
+rect_area=lenght*breath;
+printf("rect_area=%d",rect_area);
+}
