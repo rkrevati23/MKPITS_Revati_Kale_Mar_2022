1c6907d0cafab361a9a1a56641404e4b15abb334
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-03 21:04:03 +0530

    Add files via upload

---
+++ multysw.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,r;
+printf("enter value oe number/n");
+scanf("%d",&num);
+r=num%6;
+if(r==0);
+{
+printf("number multiply by 6");
+}
+if(r!=0);
+printf("number is not multiply by 6");
+getch();
+}
