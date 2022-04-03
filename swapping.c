bcf47031b98e0f9ea882daddff8951529dea8759
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-03 21:02:16 +0530

    Add files via upload

---
+++ swapping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,sw;
+printf(" enter 2 number");
+scanf("%d%d",&num1,&num2);
+sw=num2;
+num2=num1;
+printf("after swapping\n");
+scanf("num1=%dnum2=%d,&num1,&num2");
+getch();
+}
