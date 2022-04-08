237b4e8fc33e8fa47ef5461b518970d4dbecad95
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-08 20:47:36 +0530

    Add files via upload

---
+++ qu6.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+char num1='X';
+char num2='M';
+char num3='L';
+	
+	printf("the reserve number of %c%c%c is %c%c%c /n",
+	        num1,num2,num3,
+	        num3,num2,num1);
+
+}
