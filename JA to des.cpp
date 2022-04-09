1800fb559ea24cde0dc36dfdb6683f16786c40e0
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-09 12:25:01 +0530

    Add files via upload

---
+++ JA to des.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int num;
+printf("enter the number from 1 to 12");
+scanf("%d",&num);
+switch(num)	
+{
+	case 1:
+		printf("january");
+	break;
+	
+	case 2:
+	    printf("february");
+	break;
+	
+	case 3:
+		printf("march");
+	break;
+	 
+	case 4:
+	 	printf("april");
+	break;
+	
+	case 5:
+	    printf("may");
+	break;
+	
+	case 6:
+		printf("june");
+	break;
+	
+	case 7:
+		printf("july");
+	break;
+	
+	case 8:
+		printf("augest");
+	break;
+	
+	case 9:
+		printf("september");
+	break;
+	
+	case 10:
+		printf("octomber");
+	break;
+	
+	case 11:
+		printf("november");
+	break;
+	
+	case 12:
+		printf("december");
+	break;	
+	
+		printf("i am in default");
+}
+getch();
+}
