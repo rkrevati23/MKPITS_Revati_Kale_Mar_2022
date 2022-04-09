858a97a21f3c1d12d638e4b65f71febde0b84047
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-09 12:22:50 +0530

    Add files via upload

---
+++ 1qu4.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	printf("######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	
+	
+	printf("  ##########\n");
+	printf(" ##    ##\n ");
+	printf(" #\n");
+	printf(" #\n ");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" #\n");
+	printf(" ##    ##\n");
+	printf("  ##########\n");
+}
