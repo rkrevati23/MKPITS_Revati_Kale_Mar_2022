92dd21157bc43c3d36d0815a5f1692fcfc1974e0
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-09 15:45:22 +0530

    Add files via upload

---
+++ qu21.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+main()
+{
+	int p;
+	printf("\n enter number");
+	scanf("%d",&p);
+{
+}
+	
+	if(p>0 && p<=20)	
+{
+     printf("range [0to20]\n,");
+}
+ else 
+ if(p>41 && p<=60)
+ 
+{
+	printf("range 41to60\n,");
+}
+ else
+ if(p>61 && p<=80)
+{
+ 	printf("range 61to80\n,");
+ 	
+}	   
+else
+{
+	printf(" the range\n,");
+	
+}
+}
