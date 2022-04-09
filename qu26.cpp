b0813905b6a9cb6292d9ff86486a285130546fa7
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-09 12:21:10 +0530

    Add files via upload

---
+++ qu26.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i;
+	printf("even numbers between 1 to 50\n ");
+	i=1;
+{
+while(i<=50)
+{
+	    printf("%d",i);
+        i++;
+}
+}
+getch();
+}
