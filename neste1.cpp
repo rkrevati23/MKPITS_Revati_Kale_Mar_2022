5657bd822f4bee5b1d774cd6e24a69c3a223505d
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-09 15:19:59 +0530

    Add files via upload

---
+++ neste1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int p1,p2,p3;
+printf("enter the number");
+scanf("%d%d%d",&p1,&p2,&p3);
+{
+}
+if(p1>p2&&p1>p3)
+{
+    printf("enter p1");
+}
+else
+if(p2>p1&&p2>p3)
+{
+    printf("enter p2");
+}
+else
+{
+printf("enter p3");
+}
+getch();
+}
