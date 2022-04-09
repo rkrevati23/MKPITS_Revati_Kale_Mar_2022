09212b15552287bdf73431267a666ec681349c8b
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-09 12:18:57 +0530

    Add files via upload

---
+++ qu24.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ main()
+{
+	int num,r;
+	printf("enter two integer number/n");
+	scanf("%d",&num);
+	r=num*7; 
+if(r==0);
+{
+	printf("numberis multiply ");
+}
+if(r!=0);
+ 
+    printf("number is not multiply");	
+}
