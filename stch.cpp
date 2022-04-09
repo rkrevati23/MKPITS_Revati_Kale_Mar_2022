639a21c649562f6f39a24e9eab784064f5ba3c67
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-09 12:19:36 +0530

    Add files via upload

---
+++ stch.cpp
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+main()
+{
+	int num1, choice;
+	printf("enter 1  or 2 or 3");
+	scanf("%d",&choice);
+	switch(1)
+	{
+		case 1:
+			printf("in case 1");
+			break;
+		case 2:
+			printf("in case 2");
+			break;
+		case 3:
+			printf("in case 3");
+			break;
+		default:
+	printf("i am in default");	
+}
+}
