0981b72a4cdc538d6697ed8cbe0605eb9b0bc916
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-11 17:41:23 +0530

    Add files via upload

---
+++ even sqr.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,p;
+	printf("enter value");
+	scanf("%d",&p);
+	printf("square of each one of the even value from 1to a %d",p);
+	
+	while(i=2, i<=p, i++)
+     {
+ 
+     if(i%2==0)
+   {
+	  
+	 	printf("d^2=%d",i,i*i);
+   }
+  }
+       
+
+}
