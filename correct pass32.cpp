e4f226e5bfd6166a6d4e63c4d7b119926736fda4
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-12 12:43:12 +0530

    Add files via upload

---
+++ correct pass32.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x,pass;
+	
+	printf(" enter the password");	
+	
+while(x!=0);
+{
+	scanf("%d",&x);
+if(x==1234)	
+{
+	printf("correct password");
+
+}
+else
+{
+	printf("wrong password");
+}
+     x++;
+}
+}
