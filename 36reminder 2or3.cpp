3e96d7ee59a07bffa0d048e389a5221f2054cb66
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-12 12:41:08 +0530

    Add files via upload

---
+++ 36reminder 2or3.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int p,q;
+	printf("enter two numbers\n");
+	scanf("%d%d",&p,&q);
+	printf("all numbers\n");
+	while(p<=q)
+	{
+	if(p%7==2&& p%7==3)
+	{
+	printf("%d\n",&p); 	
+	p++;		   
+    }
+    }
+	     
+}
