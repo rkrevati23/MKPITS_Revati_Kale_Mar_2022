a1096317584271f15869d7ecf79c881b692febda
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-13 17:39:22 +0530

    Add files via upload

---
+++ addition of n num by loop.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int num, n1,sum=0;
+    printf("\nenter the n number");
+    scanf("%d",&num);
+    while(num>0)
+    {
+    	n1=num%10;
+    	
+    	sum=sum*10+n1;
+    	
+    	num=num/10;
+    	
+    	printf("sum=%d",sum);
+    	
+	}
+}
