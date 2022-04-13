df1f490ebf22a870b4a79a78372e1da78f2c90b1
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-13 17:46:37 +0530

    Add files via upload

---
+++ add num by loop.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,add=0;
+	i=10;
+	while(i>=1);
+    {
+	    (add=add+i);  
+	    i--;
+    }
+      	printf("%d",&add);
+        
+}
+	    
+  
