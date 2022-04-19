9c684663e4ca587f1acd21fb0158550f97b99dcc
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-19 17:22:53 +0530

    Add files via upload

---
+++ pass1234for loop.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x,pass;
+	
+	printf(" enter the password");	
+for(;x!=0;x++);
+{
+	scanf("%d",&x);
+if(x==1234)	
+{
+
+	printf("correct password");
+}
+else
+{
+	printf("wrong password");
+}
+     
+}
+}
