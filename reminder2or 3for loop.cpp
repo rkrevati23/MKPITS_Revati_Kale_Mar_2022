146a99356b2c0ed1d02f022d975fb09685bca0e6
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-19 16:53:20 +0530

    Add files via upload

---
+++ reminder2or 3for loop.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int p,q;
+	printf("enter two numbers\n");
+	scanf("%d%d",&p,&q);
+	for(;p<=q;p++)
+	{
+	
+	if(p%7==2||p%7==3);
+	
+    }
+     	printf("%d\n",&p); 	
+	    
+			   
+	
+	 
+	}
+	
+
+			   
