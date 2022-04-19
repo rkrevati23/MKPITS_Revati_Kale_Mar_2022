fa52bd9894ad7b9a34dc821bce04b7d7205813a3
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-19 17:05:55 +0530

    Add files via upload

---
+++ factorial for loop.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int num,i,fact=1;
+	printf("enter number");
+	scanf("%d",&num);
+	i=1;
+   for(i=1;i>=1;i--)
+	
+	{
+		fact=fact*i;
+			
+	}
+	printf("fact=%d",fact);
+}
