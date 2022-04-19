2a61d0bdc8a3ba6ef02cf34261434b5da17cf306
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-19 16:23:01 +0530

    Add files via upload

---
+++ n revarse for ;oop.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int num,n1,rev=0;
+	printf("enter the number of digit");
+	scanf("%d",&num);
+	int x=num;
+	for(;num>0;)
+
+{
+	n1=num%10;
+	rev=rev*10+n1;
+	num=num/10;
+	
+}
+if(x==rev);
+{
+ 
+	printf("reverse number of digit=%d",rev);
+}
+}
