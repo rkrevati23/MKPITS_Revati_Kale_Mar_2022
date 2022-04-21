c838a1508e7d45195c765ca112ade386a62fae91
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-21 16:56:20 +0530

    Add files via upload

---
+++ array2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i, a[3];
+	printf("enter 3 number\n");
+	for(i=0;i<=2;i++)
+	{
+		printf("a[%d]= ",i);
+		scanf("%d",&a[i]);
+	} 
+	for(i=0;i<=2;i++)
+	{
+		printf("a[%d]=%d\n",i, a[i]);
+	}
+	    printf("\n");
+}
