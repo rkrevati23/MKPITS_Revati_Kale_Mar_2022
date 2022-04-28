38808de3f02c5ad7c26ef3045f9047a8004d8db3
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-28 13:04:19 +0530

    Add files via upload

---
+++ len of str [10].cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char n1[10];
+	int i,cnt=0;
+	printf("enter any name\n");
+	scanf("%s",n1);
+	
+	for(i=0;n1[i]!='\0';i++)
+	{
+		cnt=cnt+1;
+	}
+	printf("lenth=%d", cnt);
+	
+
+}
