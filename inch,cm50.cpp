9024f06e05fbf1fbcb5056e8f1eab20bc8da8024
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-29 00:56:21 +0530

    Add files via upload

---
+++ inch,cm50.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	double inch,cm,inch_to_cm;
+	
+	printf("distance incm:\n");
+	scanf("%lf",&cm);
+	
+	inch=cm/inch_to_cm;
+	
+	printf("distance of %0.2lf cms is: %0.2lf inches\n",cm,inch);
+	
+}
