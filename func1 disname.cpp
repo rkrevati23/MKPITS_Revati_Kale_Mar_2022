b743176355b35ca01396296370760297e118c927
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-28 19:54:35 +0530

    Add files via upload

---
+++ func1 disname.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void disname();
+int main()
+
+
+{
+	disname();
+	disname();
+	disname();
+	disname();
+}
+void disname()
+{
+	printf("my name\n");
+	getch();
+}
