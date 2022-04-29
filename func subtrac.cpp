6c1b5e41653f46c9dac5f90f22d510cdd930b1d8
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-29 17:26:05 +0530

    no argumennt no returntype

---
+++ func subtrac.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int sub();
+int main()
+{
+
+ sub();
+}
+
+int sub()
+
+{
+  int num1,num2,sub;
+  printf("enter the number\n");
+  scanf("%d",&num1);
+  printf("enter the number\n");
+  scanf("%d",&num1);
+  
+  sub=num1-num2;
+  printf("subtraction=%d",sub);
+}
