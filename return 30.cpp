66e725e8bc467bad460bbd1fda5636f0ace2d7cf
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-29 18:03:28 +0530

    return true if one of them 30 or sum is 30

---
+++ return 30.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,sum;
+	printf("enter two numbers\n");
+	scanf("%d%d",&n1,&n2);
+	
+	sum=n1+n2;
+	if(n1==30||n2==30||sum==30)
+	{
+		printf(" return is true");
+	}
+	else
+	{
+		printf("return is not true");
+	}
+}
