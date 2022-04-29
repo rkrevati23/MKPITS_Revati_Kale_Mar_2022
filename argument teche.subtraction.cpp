f19bf511155bc7599a85f87b0425306ef20e3f05
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-29 17:30:41 +0530

    no returntype

---
+++ argument teche.subtraction.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void fun1();
+void sub(int x,int y);
+int main()
+{
+
+ fun1();
+}
+
+void fun1()
+
+{
+  int a=2,b=3;
+  
+   sub(a,b);
+   
+}
+void sub(int x,int y)
+{
+	int r;
+	r=x-y;
+	printf("%d",r);
+}
