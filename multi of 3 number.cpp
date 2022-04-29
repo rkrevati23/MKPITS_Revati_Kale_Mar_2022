8ec9f60a7acf145efbd6fa71eaae6f17139e21e9
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-29 14:31:55 +0530

    multi of three number 73

---
+++ multi of 3 number.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float a,b,c,multiply;
+	
+	printf("enter three number\n");
+	scanf("%f%f%f",&a,&b,&c);
+	
+	multiply=a*b*c;
+	
+	printf("multiplication of three number: %f",multiply);
+
+}
