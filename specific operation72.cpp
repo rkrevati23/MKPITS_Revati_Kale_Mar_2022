814603ef81a1e3c106edfe35fbd44be4e17259d2
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-29 14:20:59 +0530

    Add files via upload

---
+++ specific operation72.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,n3,n4;
+	
+	n1=-1+4*6;
+		printf("%d\n",n1);
+		
+	n2=(35+5)%7;
+		printf("%d\n",n2);
+		
+    n3=14+-4*6/11;
+	printf("%d\n",n3);
+	
+	n4=2+15/6*1-7%2;
+	printf("%d\n",n4);
+
+}
