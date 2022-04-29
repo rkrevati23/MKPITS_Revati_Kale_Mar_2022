58d2ff1327db7322d06c283d9ec6ff8f12e88a8d
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-04-29 14:33:45 +0530

    possitive num is multy of 3 or7

---
+++ multiply 3 or 7.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num;
+	printf("enter any positive number\n");
+	scanf("%d",&num);
+	
+	if(num%3==0)
+	{
+	printf("\nmultiple of 3");
+	}
+	
+	if(num%7==0)
+	{
+	printf("\nmultiple of 7");
+	}
+}
