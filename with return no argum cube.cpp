53525255ac4ce4fdaadee28c110fd93300c57423
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-05 09:01:34 +0530

    Add files via upload

---
+++ with return no argum cube.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+                                                   
+int fun( )
+{
+
+ 	int a=5,res;
+ 	
+ 	res=a*a*a;
+ 	
+ 	return res;
+ 
+ }
+int main()
+ {
+ 	int r;
+ 	
+ 	r= fun();
+ 	
+ 	printf("%d",r);
+ }
+
+	
