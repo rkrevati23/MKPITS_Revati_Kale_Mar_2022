a76ee8f03551b0455068ae0f1bca074bcf4d1d00
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-09 11:56:45 +0530

    Add files via upload

---
+++ 07length 2 contain 15 or 20.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void fun();
+int check(int n[]);
+int main()
+{
+	fun();
+}
+void fun()
+{
+	int a1[] = {12,20};
+	printf("%d", check(a1));
+	
+	int a2[] = {13,15};
+	printf("\n%d", check (a2));
+	
+	int a3[] = {11,21};
+	printf("\n%d", check(a3));
+}
+int check(int n[])
+{
+	return (n[0]==12 || n[0]==15 || n[1]==12 || n[1] == 15);
+}
