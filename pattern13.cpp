a968e78150d98e7fc3641e2131c88cc27ff43dc0
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-10 21:21:12 +0530

    EDCBA.....EDCBA

---
+++ pattern13.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() 
+                              //EDCBA
+{                             //EDCBA
+	int r,c;                  //EDCBA
+	for(r=0;r<4;r++)          //EDCBA
+	{
+		for(c=4;c>=0;c--)
+		{
+			printf("%c",(c+65));
+		}
+		printf("\n");
+	}
+}
