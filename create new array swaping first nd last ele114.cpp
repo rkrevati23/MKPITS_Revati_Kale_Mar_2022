8126d7ea0b625f53cd9e89a3e48aa8be660d7f97
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-10 17:02:29 +0530

    Add files via upload

---
+++ create new array swaping first nd last ele114.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a[20],b[20],i ,n;
+	printf("enter length of array");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d]= ",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0; i<n; i++)
+	{
+		if(i==0)
+		{
+			b[n-1]=a[i];
+		}
+		else
+		if(i==n-1)
+		{
+			b[0]=a[n-1];
+		}
+		else
+		{
+			b[i]=a[i];
+		}
+	}
+	for(i=0; i<n; i++)
+	{
+		printf("b[%d]=%d\n",i,b[i]);
+	}
+}
