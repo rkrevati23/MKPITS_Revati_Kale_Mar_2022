9973ba07206fd671473088cf439f8e1583ca0c06
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-10 23:07:00 +0530

    pattern19
    55555
    4444
    333
    22
    1

---
+++ pattern19.cpp
@@ -0,0 +1,22 @@
+//55555
+//4444
+//333
+//22
+//1
+   
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c;
+	for(r=5;r>=1;r--)
+	{
+		for(c=1;c<=r;c++)
+		{
+			
+			printf("%d",r);	
+		
+		}
+		printf("\n");	
+	}	
+}
