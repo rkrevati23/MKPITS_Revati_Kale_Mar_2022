9a0d990ee31bef4d8a13c21c0afbefc615aea416
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-17 21:58:18 +0530

    create new array taking ele before element value 5 of integer

---
+++ 136taking ele  before ele value 5.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int a[10],i,n,s=0;
+  printf("enter the number of array :");
+  scanf("%d",&n);
+  
+  printf("enter the array\n");
+  for(i=0;i<n;i++)
+  {
+  printf("a[%d]= ",i);
+  scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+   if(a[i]==5)
+   {
+     s=s+1;
+     break;
+     }
+   else
+   {
+   s=s+1;
+   }
+
+  }
+  printf("new array\n");
+  for(i=s;i<n;i++)
+  {
+  printf("a[%d]=%d\n",i,a[i]);
+  }
+  getch();
+  } 
