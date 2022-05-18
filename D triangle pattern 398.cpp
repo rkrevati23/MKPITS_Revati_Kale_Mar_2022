57a811154a1ea13c97c3555e03252c51e93e2198
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-18 12:50:26 +0530

    triangle pattern 398 star

---
+++ D triangle pattern 398.cpp
@@ -0,0 +1,38 @@
+// * * * * * *
+//  * * * * *
+//    * * * 
+//      *
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,n;
+	for(r=4;r>=1;r--)
+	{
+		for(c=1;c<=4;c++)
+		{
+			if(r+c>4)
+			{
+				printf( "* ");
+			}
+			else
+			{
+			printf("  ");
+		    }
+	   }
+		for(n=3; n>=1; n--)
+		{
+			if(r+n>4)
+			{
+				printf("* ");
+
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+	printf("\n");
+	}
+}
