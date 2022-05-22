9a02d461daa12cb9a45d8bf92c537cd2c097cba8
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-22 12:09:20 +0530

    display the value n is 1 when m is greater than 0, 0 when m is 0 nd -1 when m is less than 0

---
+++ 150value n is whenm is larger than0.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int m,n;
+    printf("enter the value of m: ");
+    scanf("%d",&m);
+  
+  if(m>0)
+  {
+    printf("n=1");	
+  }
+   else if(m==0)
+  {
+   printf("n=0");	
+  }
+  else
+  {
+   printf("n=-1");	
+  }	
+}
