0429ee1209e82f70db42f5cde8f9fc194d054155
rkrevati23 <102286720+rkrevati23@users.noreply.github.com>
2022-05-22 12:02:01 +0530

    take number as input then display a rectangle of 3 column wide nd 5 row tall using that digit

---
+++ 146 rectangle 3 column wide nd 5 row tall.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int x ;
+   printf("enter any number: ");
+   scanf("%d",&x);
+   
+   printf("%d %d %d %d\n",x,x,x);
+   printf("%d     %d\n",x,x);
+   printf("%d     %d\n",x,x);
+   printf("%d     %d\n",x,x);
+   printf("%d %d %d %d\n",x,x,x);
+
+}
